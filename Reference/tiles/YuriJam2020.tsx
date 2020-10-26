10585ddf85bc2f4ec691562dff00a43da7dc1c26
Paige Ashlynn <mx.ashlynn@gmail.com>
2020-10-26 16:40:59 -0700

    Sets up tileset image.

--- Reference/tiles/YuriJam2020.tsx
+++ Reference/tiles/YuriJam2020.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="YuriJam2020" tilewidth="16" tileheight="16" tilecount="1" columns="1" backgroundcolor="#eff8e9" objectalignment="center">
- <image source="sample.png" trans="65ff00" width="16" height="16"/>
+<tileset version="1.4" tiledversion="1.4.2" name="YuriJam2020" tilewidth="16" tileheight="16" tilecount="128" columns="16" backgroundcolor="#eff8e9" objectalignment="center">
+ <image source="tileset.png" trans="65ff00" width="256" height="128"/>
 </tileset>
