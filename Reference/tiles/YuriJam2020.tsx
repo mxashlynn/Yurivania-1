5a5b1a72303fc1e1ce9c60048ef0d5c7983964ef
MxAshlynn <mx.ashlynn@gmail.com>
2020-10-28 15:38:14 -0700

    Updates Tiled tileset.

--- Reference/tiles/YuriJam2020.tsx
+++ Reference/tiles/YuriJam2020.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="YuriJam2020" tilewidth="16" tileheight="16" tilecount="128" columns="16" backgroundcolor="#eff8e9" objectalignment="center">
- <image source="tileset.png" trans="65ff00" width="256" height="128"/>
+<tileset version="1.4" tiledversion="1.4.2" name="Yuri Jam 2020" tilewidth="8" tileheight="8" tilecount="180" columns="18" backgroundcolor="#65ff00" objectalignment="center">
+ <image source="TilePatternTable.png" trans="65ff00" width="144" height="80"/>
 </tileset>
