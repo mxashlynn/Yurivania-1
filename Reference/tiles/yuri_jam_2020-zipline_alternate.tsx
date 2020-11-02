e83b7636c0279e170383a51e8ebd81cb5e7a166e
Paige Ashlynn <mx.ashlynn@gmail.com>
2020-11-01 18:49:21 -0800

    Adds an alternate tileset to accommodate the zipline.

---
+++ Reference/tiles/yuri_jam_2020-zipline_alternate.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="Yuri Jam 2020 - Zipline Alternate" tilewidth="8" tileheight="8" tilecount="180" columns="18" backgroundcolor="#65ff00">
+ <image source="tile_pattern_table-zipline.png" trans="65ff00" width="144" height="80"/>
+</tileset>
